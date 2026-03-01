4df04c77661b76a211244e78e73041e2b2c07710
GarvvChaudhary <garvchaudhary0345@gmail.com>
2026-03-01 20:47:00 +0530

    Add files via upload

---
+++ Day 4/demo.py
@@ -0,0 +1,27 @@
+# print('Hello, World!')
+# print('This is a demo script.')
+# a,b = int(input()), int(input())
+# print(a + b)
+# print('Hello, World!')
+# print('This is a demo script.')
+# a,b = int(input()), int(input())
+# print(a + b)
+# print('Hello, World!')
+# print('This is a demo script.')
+# a,b = int(input()), int(input())
+# print(a + b)
+# print('Hello, World!')
+# print('This is a demo script.')
+# a,b = int(input()), int(input())
+# print(a + b)
+# print('Hello, World!')
+# print('This is a demo script.')
+# a,b = int(input()), int(input())
+# print(a + b)
+
+def addTwoNums(a, b):
+    # print(a+b)
+    return a + b
+
+print(addTwoNums(3, 5)) 
+
